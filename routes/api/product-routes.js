25b564282cddbe73b294bed1647288b709d5e51e
[carlos martinez] <theycallhellp@gmail.com>
2022-11-03 18:02:36 -0700

    add get all api route functionality

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -55,7 +55,7 @@ router.put('/:id', (req, res) => {
       id: req.params.id,
     },
   })
-    .then((product) => {
+    .then(() => {
       // find all associated tags from ProductTag
       return ProductTag.findAll({ where: { product_id: req.params.id } });
     })
