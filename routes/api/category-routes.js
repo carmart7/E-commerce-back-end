2d09c256bb1e978cb937582a6e09a0c2fcc8ead5
[carlos martinez] <theycallhellp@gmail.com>
2022-11-03 18:27:02 -0700

    add delete by id route functionality

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -49,8 +49,14 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({where: {id: req.params.id}});
+    res.status(200).json(categoryData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
